2cca8fefd43112b7961bddbe1e22d297a5284010
M Hamza Shakeel <55921857+hamzashakeel348@users.noreply.github.com>
2021-10-08 03:09:11 +0500

    Material UI Table

--- coreui-free-react-admin-template-4/src/components/table.js
+++ coreui-free-react-admin-template-4/src/components/table.js
@@ -1,54 +1,38 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
-import BootstrapTable from 'react-bootstrap-table-next'
-import paginationFactory from 'react-bootstrap-table2-paginator'
-import filterFactory, { textFilter } from 'react-bootstrap-table2-filter'
-import 'react-bootstrap-table2-filter/dist/react-bootstrap-table2-filter.min.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
 
-function Tables() {
+import MaterialTable from 'material-table'
+
+const Table = () => {
   const [users, setusers] = useState([])
-
   const columns = [
-    { dataField: 'id', text: 'Id', sort: true },
-    { dataField: 'name', text: 'Name', sort: true },
-    {
-      dataField: 'username',
-      text: 'Username',
-      sort: true,
-      filter: textFilter(),
-    },
-    { dataField: 'email', text: 'Email', sort: true },
+    { title: 'Id', field: 'id', filtering: false },
+    { title: 'Name', field: 'name' },
+    { title: 'Username', field: 'username' },
+    { title: 'Email', field: 'email' },
   ]
-
-  const paginator = paginationFactory({
-    page: 1,
-    sizePerPage: 5,
-    lastPageText: '>>',
-    firstPageText: '<<',
-    nextPageText: '>',
-    prePageText: '<',
-    showTotal: true,
-    alwaysShowAllBtns: true,
-  })
 
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
       .then((result) => setusers(result))
       .catch((error) => console.log(error))
-  })
+  }, [])
+
   return (
-    <div>
-      <BootstrapTable
-        bootstrap4
-        pagination={paginator}
-        keyField="id"
+    <div style={{ maxWidth: '100%' }}>
+      <MaterialTable
+        title="table"
         columns={columns}
         data={users}
-        filter={filterFactory()}
+        options={{
+          exportButton: true,
+          sorting: true,
+          filtering: true,
+        }}
       />
     </div>
   )
 }
-export default Tables
+
+export default Table
--- coreui-free-react-admin-template-4/src/layout/DefaultLayout.js
+++ coreui-free-react-admin-template-4/src/layout/DefaultLayout.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { AppContent, AppSidebar, AppFooter, AppHeader } from '../components/index'
-import Table from '../components/table'
+import Table from '../components/Table'
 const DefaultLayout = () => {
   return (
     <div>
